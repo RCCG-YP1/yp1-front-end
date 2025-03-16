c8a42bc521930b32ade3bc38086ffa47812be3f9
godbrand <thompsoneregha005@gmail.com>
2025-03-16 16:37:14 -0700

    `Added data for provincial leadership, zonal pastors, and area pastors`

--- src/pages/province/index.jsx
+++ src/pages/province/index.jsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
 import { SearchIcon } from "@/assets/icons";
+const pastorsData = {
+  provincialLeadership: [
+    {
+      id: 1,
+      name: "Pastor John Doe",
+      role: "Provincial Overseer",
+      location: "New York Province",
+      image: "public/images/pst_leke.jpeg",
+    },
+    {
+      id: 2,
+      name: "Pastor James Smith",
+      role: "Assitant Overseer",
+      location: "New York Province",
+      image: "public/images/pst_white.jpeg",
+    },
+    {
+      id: 3,
+      name: "Pastor Andrew Clark",
+      role: "Executive Secretary",
+      location: "New York Province",
+      image: "public/images/pst_white.jpeg",
+    },
+  ],
+
+  ZonalPastors: [
+    { id: 4, name: "Pastor Charles Johnson", zone: "Zone A" },
+    { id: 5, name: "Pastor David Brown", zone: "Zone B" },
+    { id: 6, name: "Pastor Michael Davis", zone: "Zone C" },
+    { id: 7, name: "Pastor George Brown", zone: "Zone D" },
+    { id: 8, name: " Pastor William Davis", zone: "Zone E" },
+  ],
+
+  areaPastors: [
+    { id: 9, name: "Pastor Samuel Roberts", area: "Area 1" },
+    { id: 10, name: "Pastor Matthew Lewis", area: "Area 2" },
+    { id: 11, name: "Pastor Steven Walker", area: "Area 3" },
+    { id: 12, name: "Pastor Joseph Hall", area: "Area 4" },
+  ],
+};
 
 export default function Province() {
   const [activeTab, setActiveTab] = useState("leadership");
   const [activeButton, setActiveButton] = useState("zone");
+  const { provincialLeadership, ZonalPastors, areaPastors } = pastorsData;
   return (
     <div>
       <div>
@@ -44,13 +85,49 @@ export default function Province() {
         {activeTab === "leadership" && (
           <div>
             <div className="mt-7">
-              <h2>Provincial Leadership</h2>
+              <h2 className="text-2xl font-bold mb-4">Provincial Leadership</h2>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {provincialLeadership.map((pastor) => (
+                  <div
+                    key={pastor.id}
+                    className=" rounded-lg relative shadow-md "
+                  >
+                    <img
+                      src={pastor.image}
+                      alt={pastor.name}
+                      className="w-full h-full relative object-cover rounded-lg"
+                    />
+                    <div className="absolute top-52 left-0 text-green-400">
+                      <h3 className="text-lg font-semibold mt-2">
+                        {pastor.name}
+                      </h3>
+                      <p className="text-sm">{pastor.role}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
             <div>
-              <h2>Zonal Pastors</h2>
+              <h2 className="text-2xl font-bold mt-6 mb-4">Zonal Pastors</h2>
+              <ul>
+                {ZonalPastors.map((pastor) => (
+                  <li key={pastor.id} className="py-2 ">
+                    <div>{pastor.name}</div>
+                    <div className="text-green-400">{pastor.zone}</div>
+                  </li>
+                ))}
+              </ul>
             </div>
             <div>
-              <h2>Area Pastors</h2>
+              <h2 className="text-2xl font-bold mt-6 mb-4">Area Pastors</h2>
+              <ul>
+                {areaPastors.map((pastor) => (
+                  <li key={pastor.id} className="py-2 ">
+                    <div>{pastor.name}</div>{" "}
+                    <div className="text-green-400">{pastor.area}</div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         )}
